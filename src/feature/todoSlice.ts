94daf2fe241633d791f1ec49e48fcf1561d0d267
faizalfakhri0001 <faizalfakhri0001@gmail.com>
2022-08-31 10:59:22 +0700

    test

--- src/feature/todoSlice.ts
+++ src/feature/todoSlice.ts
@@ -1,4 +1,4 @@
-import {createSlice , createAsyncThunk, createEntityAdapter} from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk, createEntityAdapter, PayloadAction } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { RootState } from '../app/store';
 import { Post, Posts } from '../types';
@@ -36,22 +36,35 @@ export const getTodo = createAsyncThunk("todos/getTodo", async (_, thunkApi) => 
 });
 
 export const addTodo = createAsyncThunk("todos/addTodo", async (todo: Omit<Post, 'id'>, thunkApi) => {
-    const res = await axios.post<Post>(`${URL}/todos`, todo).catch((err) =>{
+    const res = await axios.post<Post>(`${URL}/todos`, todo).catch((err) => {
         thunkApi.rejectWithValue(err);
         throw err
     });
     return res.data;
 });
 
-export const deleteTodo = createAsyncThunk("todos/deleteTodo", async(todoId:number, thunkApi) =>{
+export const deleteTodo = createAsyncThunk("todos/deleteTodo", async (todoId: number, thunkApi) => {
     const res = await axios.delete<Post>(`${URL}/todos/${todoId}`, {
-        data: {id : todoId},
-    }).catch((err) =>{
+        data: { id: todoId },
+    }).catch((err) => {
         thunkApi.rejectWithValue(err);
         throw err;
     });
-    return {data: res.data, todoId};
+    return { data: res.data, todoId };
 })
+
+const date = new Date();
+export const updateTodo = createAsyncThunk("todos/updateTodo", async (todo: Partial<Post>, thunkApi) => {
+    const res = await axios.patch<Post>(`${URL}/todos/${todo.id}`, {
+        title: todo.title,
+        date: date.toLocaleDateString(),
+        note: todo.note
+    }).catch((err) => {
+        thunkApi.rejectWithValue(err)
+        throw err;
+    });
+    return res.data;
+});
 
 
 const todoSlice = createSlice({
@@ -60,48 +73,64 @@ const todoSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-        .addCase(getTodo.pending, (state) =>{
-            state.status = 'loading';
-            state.message = '';
-        })
-        .addCase(getTodo.rejected, (state, action) =>{
-            state.status = 'failed';
-            if(action.error.message){
-                state.message = action.error.message;
-            }
-        })
-        .addCase(getTodo.fulfilled, (state, action) =>{
-            state.status = 'success bro';
-            postsAdapter.setAll(state, action.payload)
-        })
-        .addCase(addTodo.pending, (state) =>{
-            state.status = 'loading';
-            state.message='';
-        })
-        .addCase(addTodo.rejected, (state, action) =>{
-            state.status = 'failed';
-            if(action.error.message){
-                state.message = action.error.message;
-            }
-        })
-        .addCase(addTodo.fulfilled, (state, action) =>{
-            state.status = 'success bro';
-            postsAdapter.addOne(state, action.payload)
-        })
-        .addCase(deleteTodo.pending, (state) =>{
-            state.status = 'loading';
-            state.message='';
-        })
-        .addCase(deleteTodo.rejected, (state, action) =>{
-            state.status = 'failed';
-            if(action.error.message){
-                state.message = action.error.message;
-            }
-        })
-        .addCase(deleteTodo.fulfilled, (state, action) =>{
-            state.status = 'success bro';
-            postsAdapter.removeOne(state, action.payload.todoId);
-        })
+            .addCase(getTodo.pending, (state) => {
+                state.status = 'loading';
+                state.message = '';
+            })
+            .addCase(getTodo.rejected, (state, action) => {
+                state.status = 'failed';
+                if (action.error.message) {
+                    state.message = action.error.message;
+                }
+            })
+            .addCase(getTodo.fulfilled, (state, action) => {
+                state.status = 'success bro';
+                postsAdapter.setAll(state, action.payload)
+            })
+            .addCase(addTodo.pending, (state) => {
+                state.status = 'loading';
+                state.message = '';
+            })
+            .addCase(addTodo.rejected, (state, action) => {
+                state.status = 'failed';
+                if (action.error.message) {
+                    state.message = action.error.message;
+                }
+            })
+            .addCase(addTodo.fulfilled, (state, action) => {
+                state.status = 'success bro';
+                postsAdapter.addOne(state, action.payload)
+            })
+            .addCase(deleteTodo.pending, (state) => {
+                state.status = 'loading';
+                state.message = '';
+            })
+            .addCase(deleteTodo.rejected, (state, action) => {
+                state.status = 'failed';
+                if (action.error.message) {
+                    state.message = action.error.message;
+                }
+            })
+            .addCase(deleteTodo.fulfilled, (state, action) => {
+                state.status = 'success bro';
+                postsAdapter.removeOne(state, action.payload.todoId);
+            })
+            .addCase(updateTodo.pending, (state) => {
+                state.message = '';
+            })
+            .addCase(updateTodo.rejected, (state, action) => {
+                if (action.error.message) {
+                    state.message = action.error.message;
+                }
+            })
+            .addCase(updateTodo.fulfilled, (state, action: PayloadAction<Post>) => {
+                state.status = 'idle';
+                const { id, ...updateData } = action.payload;
+                postsAdapter.updateOne(state, {
+                    id: action.payload.id,
+                    changes: { ...updateData },
+                });
+            })
     },
 });
 
--- src/pages/EditTodo.tsx
+++ src/pages/EditTodo.tsx
@@ -1,60 +1,60 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../app/hook";
 import { useNavigate, useParams } from "react-router-dom";
-import { useAppDispatch } from "../app/hook";
-import { addTodo } from "../feature/todoSlice";
-
-const current = new Date();
-const date = `${current.getDate()}/${
-  current.getMonth() + 1
-}/${current.getFullYear()}`;
-
-const defaultData = {
-  id: Math.floor(Math.random() * Date.now()),
-  title: "",
-  date: date,
-  note: "",
-};
+import { getTodo, todoSelector, updateTodo } from "../feature/todoSlice";
 
 const EditTodo = () => {
-  const [formTodo, setFormTodo] = useState(defaultData);
-  const { title, note } = formTodo;
+  const [title, setTitle] = useState("");
+  const [note, setNote] = useState("");
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const { id } = useParams();
+  const { id } = useParams() as { id: string };
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormTodo((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-  };
+  const todos = useAppSelector((state) => todoSelector.selectById(state, id));
 
-  const handleUpdate = async (e: React.FormEvent<HTMLFormElement>) => {
+  useEffect(() => {
+    dispatch(getTodo());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (todos) {
+      setTitle(todos.title);
+      setNote(todos.note);
+    }
+  }, [todos]);
+
+  const handleEdit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(formTodo);
-    await dispatch(addTodo(formTodo));
+    await dispatch(updateTodo({ title, note }));
     navigate("/");
-    setFormTodo(defaultData);
   };
 
   return (
     <div className="w-full flex flex-col items-center mt-10">
-      <h1 className="font-inter text-3xl font-bold">Add Todo</h1>
+      <h1 className="font-inter text-3xl font-bold">Edit Todo</h1>
       <form
-        onSubmit={handleUpdate}
+        onSubmit={handleEdit}
         className="mt-10 flex flex-col p-10 bg-black space-y-5 w-[30%] rounded-md shadow-md"
       >
         <input
+          className="p-2 rounded-md shadow-[1px_1px_10px_rgb(250,250,250)]"
           type="text"
           id="title"
           placeholder="Title"
           value={title}
-          onChange={onChange}
+          onChange={(e) => setTitle(e.target.value)}
         />
-        <input id="note" placeholder="Note" value={note} onChange={onChange} />
+        <input
+          className="p-2 rounded-md shadow-[1px_1px_10px_rgb(250,250,250)]"
+          id="note"
+          placeholder="Note"
+          value={note}
+          onChange={(e) => setNote(e.target.value)}
+        />
         <div className="w-full flex justify-end">
-          <button className="bg-white px-5 rounded-md">Add Note</button>
+          <button className="border-2 text-white border-white px-5 py-1 rounded-md shadow-[1px_1px_10px_rgb(250,250,250)] hover:bg-white hover:text-black font-inter font-bold">
+            Edit Todo
+          </button>
         </div>
       </form>
     </div>
